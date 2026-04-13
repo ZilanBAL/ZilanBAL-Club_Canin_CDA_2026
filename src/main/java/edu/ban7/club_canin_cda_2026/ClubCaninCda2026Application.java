93344d56d53a2b75a42bcc76ac35d74f7985a0f9
Zilo <zilanbal@yahoo.fr>
2026-04-13 09:55:35 +0200

    first commit

---
+++ src/main/java/edu/ban7/club_canin_cda_2026/ClubCaninCda2026Application.java
@@ -0,0 +1,13 @@
+package edu.ban7.club_canin_cda_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubCaninCda2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubCaninCda2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/ban7/club_canin_cda_2026/ClubCaninCda2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ban7.club_canin_cda_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubCaninCda2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
